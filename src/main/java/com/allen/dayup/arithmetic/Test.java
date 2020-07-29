1a30000b77c32611dd47fe87705e868e63e2e5fb
王伦 <872062426@qq.com>
2020-07-29 11:01:44 +0800

    feat:测试合并

--- src/main/java/com/allen/dayup/arithmetic/Test.java
+++ src/main/java/com/allen/dayup/arithmetic/Test.java
@@ -1,16 +1,8 @@
 package com.allen.dayup.arithmetic;
 
-import com.allen.dayup.guava.User;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.zip.Deflater;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * @Auther: 20190598
@@ -20,13 +12,27 @@ import java.util.zip.Deflater;
 public class Test {
 
     public static void main(String[] args) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        String json = "{\"name\":\"\",\"age\":12}";
-        JsonNode jsonNode = mapper.readTree(json);
-        System.out.println(jsonNode.get("name").asText().isEmpty());
-        System.out.println(jsonNode.get("test"));
+        //ObjectMapper mapper = new ObjectMapper();
+        //String json = "{\"name\":\"\",\"age\":12}";
+        //JsonNode jsonNode = mapper.readTree(json);
+        //System.out.println(jsonNode.get("name").asText().isEmpty());
+        //System.out.println(jsonNode.get("test"));
 
+        //ObjectMapper mapper = new ObjectMapper();
+        //String json = "{\"name\":\"allen\"}";
+        //JsonNode node = mapper.readTree(json);
+        //
+        //System.out.println(mapper.readValue(node.toString(), HashMap.class));
+
+        int starttimeInt = 2200;
+        int endtimeInt = 800;
+        int nowTimeInt = 900;
+
+         boolean result = (starttimeInt < endtimeInt && (starttimeInt <= nowTimeInt && nowTimeInt <= endtimeInt)) || (starttimeInt > endtimeInt && (starttimeInt <= nowTimeInt || nowTimeInt <= endtimeInt));
+        System.out.println(result);
     }
+
+
 
     private static void count(int a){
         a++;
