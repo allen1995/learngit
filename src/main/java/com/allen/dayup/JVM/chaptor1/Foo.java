0aa72349b7ea55be554d398535aeb32a47bde212
王伦 <872062426@qq.com>
2021-06-07 17:30:34 +0800

    feat:reduce demo

---
+++ src/main/java/com/allen/dayup/JVM/chaptor1/Foo.java
@@ -0,0 +1,9 @@
+package com.allen.dayup.JVM.chaptor1;
+
+public class Foo {
+    public static void main(String[] args) {
+        boolean flag = true;
+        if (flag) System.out.println("Hello, Java!");
+        if (flag == true) System.out.println("Hello, JVM!");
+    }
+}
---
+++ src/main/java/com/allen/dayup/java8/stream/ReduceDemo.java
@@ -0,0 +1,67 @@
+package com.allen.dayup.java8.stream;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.mapping;
+
+/**
+ * @Auther: 20190598
+ * @Date: 2021/6/3 14:53
+ * @Description:
+ */
+public class ReduceDemo {
+
+    public static void main(String[] args) {
+        List<Map<String,Object>> eventlist = new ArrayList<>();
+        Map<String,Object> event1 = new HashMap<>();
+        event1.put("eventcode","DoorOpen");
+        event1.put("total", 10);
+        event1.put("unread",1);
+        eventlist.add(event1);
+
+        Map<String,Object> event2 = new HashMap<>();
+        event2.put("eventcode","DoorOpen2");
+        event2.put("total", 10);
+        event2.put("unread",1);
+        eventlist.add(event2);
+
+        Map<String,Object> event3 = new HashMap<>();
+        event3.put("eventcode","alarm");
+        event3.put("total", 10);
+        event3.put("unread",1);
+        eventlist.add(event3);
+
+        Map<String,Long> groupEvent = eventlist.stream()
+                .map( event -> {
+                    String identifier = event.get("eventcode").toString().split("\\d+$")[0];
+                    event.put("eventcode", identifier);
+                    return event;
+                })
+                .collect(Collectors.groupingBy( map -> getEventcode(map), Collectors.counting() ));
+
+        //Map<String,Long> unreadCount = eventlist.stream()
+        //        .map( event -> {
+        //            String identifier = event.get("eventcode").toString().split("\\d+$")[0];
+        //            event.put("eventcode", identifier);
+        //            return event;
+        //        })
+        //        .collect(Collectors.groupingBy( map -> getEventcode(map),  ));
+
+        System.out.println(groupEvent);
+
+    }
+
+    private static Long mapToTotal(Map<String,Object> map) {
+        Long total = (Long) map.get("total");
+        return total;
+    }
+
+    private static String getEventcode(Map<String,Object> map) {
+        return map.get("eventcode").toString();
+    }
+}
